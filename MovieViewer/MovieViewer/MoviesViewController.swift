925f927781dc7647d6cda3f2ce82e138ca2c7953
Vivian Pham <Vivian@vivians-mbp.dynamic.ucsd.edu>
2017-01-28 21:57:02 -0800

    Added tableView

---
+++ MovieViewer/MovieViewer/MoviesViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MoviesViewController.swift
+//  MovieViewer
+//
+//  Created by Vivian Pham on 1/28/17.
+//  Copyright © 2017 Vivian Pham. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        
+        cell.textLabel?.text = "row\(indexPath.row)"
+        print("row\(indexPath.row)")
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
